3fa9031828bc67381d822d0898c851cebec5cebf
luigiolivi <eterowiczluigi@gmail.com>
2024-02-13 22:13:21 -0300

    eslint and prettier incremented

--- src/app.js
+++ src/app.js
@@ -2,20 +2,20 @@ import express from 'express'
 import routes from './routes'
 
 class App {
-    constructor() {
-        this.app = express()
+  constructor() {
+    this.app = express()
 
-        this.middlewares()
-        this.routes()
-    }
+    this.middlewares()
+    this.routes()
+  }
 
-    middlewares() {
-        this.app.use(express.json())
-    }
+  middlewares() {
+    this.app.use(express.json())
+  }
 
-    routes() {
-        this.app.use(routes)
-    }
+  routes() {
+    this.app.use(routes)
+  }
 }
 
 export default new App().app
