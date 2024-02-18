4067b2e2b544da3274f64acc8c04a381e7d1fcb9
luigiolivi <eterowiczluigi@gmail.com>
2024-02-18 14:04:38 -0300

    sequelize incremented and setted up. Furthermore, a Docker container and a postgres database has been created

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'postgres',
+    database: 'maris',
+    define: {
+        timestamps: true,
+        underscore: true,
+        underscoreAll: true
+    }
+}
