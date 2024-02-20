d2a72c072d7b813225d873a6191fa60e95bd8517
luigiolivi <eterowiczluigi@gmail.com>
2024-02-20 19:27:19 -0300

    Model setted up, uuid incremented

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express'
+import { v4 } from 'uuid'
+
+import User from './app/models/User'
 
 const routes = new Router()
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Lulu' })
+routes.get('/', async (req, res) => {
+
+  const user = await User.create({
+    id: v4(),
+    name: 'teste',
+    email: 'teste@gmail.com',
+    password_hash: 'l1j2k38sda912j3'
+  })
+  return res.json(user)
 })
 
 export default routes
