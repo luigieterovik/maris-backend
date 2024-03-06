a1a8dbf5856c2d79249d847d6fcd36a3deb9a616
luigiolivi <eterowiczluigi@gmail.com>
2024-03-06 17:28:52 -0300

    Migration add column category_id

---
+++ src/database/migrations/20240306200943-add-category-column.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Products', 'categoryId', { 
+      type: Sequelize.INTEGER, 
+      allowNull: true, 
+      references: { model: 'Categories', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL'
+    });
+  },
+
+  async down (queryInterface, ) {
+    await queryInterface.removeColumn('Products', 'category_id');
+  }
+};
