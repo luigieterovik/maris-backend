ca29fd52498d917048508693c4aa96efd0061ddd
Luigi  Olivi <eterowiczluigi@gmail.com>
2024-08-13 22:54:50 -0300

    external_reference unique

--- src/database/migrations/20240810205150-pending-orders.js
+++ src/database/migrations/20240810205150-pending-orders.js
@@ -14,6 +14,7 @@ module.exports = {
       external_reference: {
         type: Sequelize.STRING,
         allowNull: false,
+        unique: true,
       },
 
       email: {
--- src/database/scripts/deletePendingOrders.js
+++ src/database/scripts/deletePendingOrders.js
@@ -1,5 +1,23 @@
 import cron from 'node-cron'
+import { Sequelize } from 'sequelize'
+import config from '../../config/database'
 
-cron.schedule(`*/1 * * * *`, async () => {
-  console.log('running your task...')
+const sequelize = new Sequelize(config.url)
+
+const deleteOldRecords = async () => {
+  try {
+    const response = await sequelize.query(`
+      DELETE FROM PendingOrders
+      WHERE createdAt <= NOW() - INTERVAL '1 minute';
+    `)
+    console.log(response)
+    console.log('Registros antigos deletados com sucesso.')
+  } catch (error) {
+    console.error('Erro ao deletar registros:', error)
+  }
+}
+
+cron.schedule('*/1 * * * *', () => {
+  console.log('Executando tarefa agendada para deletar registros antigos')
+  deleteOldRecords()
 })
