c22bfae09d02cbb31305ebce29d57bad0c031d5e
Luigi  Olivi <eterowiczluigi@gmail.com>
2024-10-16 22:07:58 -0300

    a

--- src/app/controllers/PaymentController.js
+++ src/app/controllers/PaymentController.js
@@ -9,65 +9,6 @@ import { v4 } from 'uuid'
 dotenv.config()
 
 class PaymentController {
-  /*
-  async mercadopago(req, res) {
-    const schema = Yup.object().shape({
-      title: Yup.string().required(),
-      quantity: Yup.number().required(),
-      unit_price: Yup.number().required(),
-    })
-
-    try {
-      await schema.validateSync(req.body, { abortEarly: false })
-    } catch (err) {
-      return res.status(400).json({ error: err.errors })
-    }
-
-    const { title, quantity, unit_price } = req.body
-
-    const client = new MercadoPagoConfig({
-      accessToken: process.env.ACCESS_TOKEN_MERCADOPAGO,
-    })
-
-    try {
-      const body = {
-        items: [
-          {
-            title,
-            quantity,
-            unit_price,
-            currency_id: 'BRL',
-          },
-        ],
-        back_urls: {
-          success: 'http://localhost:3000',
-          failure: 'http://localhost:3000',
-          pending: 'http://localhost:3000',
-        },
-        auto_return: 'approved',
-      }
-
-      const preference = new Preference(client)
-      const result = await preference.create({ body })
-
-      if (result && result.id) {
-        return res.status(200).json({
-          id: result.id,
-        })
-      } else {
-        return res.status(500).json({
-          error: 'Failed to create preference',
-        })
-      }
-    } catch (err) {
-      console.log(err)
-      return res.status(500).json({
-        error: 'Failed to create preference',
-      })
-    }
-  }
-    */
-
   async pix(req, res) {
     const client = new MercadoPagoConfig({
       accessToken: process.env.ACCESS_TOKEN_MERCADOPAGO,
@@ -200,11 +141,11 @@ class PaymentController {
   }
 }
 
-const createPendingOrder = async (external_reference, pendingDeliveryId) => {
-  const pendingOrderResponse = await PendingOrder.create({
-    external_reference,
-    pendingDeliveryId,
-  })
-}
+// async function createPendingOrder(external_reference, pendingDeliveryId) {
+//   const pendingOrderResponse = await PendingOrder.create({
+//     external_reference,
+//     pendingDeliveryId,
+//   })
+// }
 
 export default new PaymentController()
