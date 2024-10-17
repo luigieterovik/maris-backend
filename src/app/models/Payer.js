a789a1ec9eb78d7ded3a26e47f548ad72bf0f460
Luigi  Olivi <eterowiczluigi@gmail.com>
2024-10-16 21:07:38 -0300

    fixed error

--- src/app/models/Payer.js
+++ src/app/models/Payer.js
@@ -4,7 +4,6 @@ class Payer extends Model {
   static init(sequelize) {
     super.init(
       {
-        id: Sequelize.INTEGER,
         name: Sequelize.STRING,
         cpf: Sequelize.STRING,
         email: Sequelize.STRING,
