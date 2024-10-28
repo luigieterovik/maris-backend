141a697a5bd19e2bbe5f5ecb1654ffdfef9cab78
Luigi  Olivi <eterowiczluigi@gmail.com>
2024-10-27 22:04:42 -0300

    table name

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,6 @@ const configDatabase = {
   dialect: 'postgres',
   url: 'postgresql://postgres:rAdFETUYpCCmfSSODPZFKPcNRdyGdTdI@autorack.proxy.rlwy.net:24767/railway',
   define: {
-    freezeTableName: true,
     timestamps: true,
     underscore: true,
     underscoreAll: true,
